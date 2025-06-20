ddf6856339ad59f895c8d5dc889c284af1d83c1d
もいずる <momizibe@outlook.jp>
2025-06-21 00:17:33 +0900

    調整

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,13 @@
 </script>
 
 <svelte:head>
+	<meta property="og:url" content="https://tools.moizlu.com/" />
+	<meta property="og:type" content="profile" />
+	<meta property="og:title" content="ツール集 | もいずる" />
+	<meta property="og:image" content="https://tools.moizlu.com/ogp.png" />
+	<meta property="og:site_name" content="ツール集 | もいずる" />
+	<meta property="og:description" content="ツール集" />
+
 	<title>ツール集 | もいずる</title>
 </svelte:head>
 
