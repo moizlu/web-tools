79bb190f1bcfdc6b244975e87cb39c11c0fa916c
もいずる <momizibe@outlook.jp>
2026-02-25 02:09:36 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
     <title>Webツール | moizlu</title>
 </svelte:head>
 
-<main class="px-5 mx-auto w-full max-w-100 h-full min-h-svh flex-col-center">
+<main class="px-5 mx-auto w-full max-w-100 h-full min-h-svh flex-col-center gap-5">
     {#each tools as tool}
         <a href={tool.url} class="w-full p-2 flex-center button-general text-2xl">
             <SvgIcon Svg={tool.icon} size={40} class="" />
